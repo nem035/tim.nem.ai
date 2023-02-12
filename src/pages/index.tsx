e667fc902b9946adb9bf6a39f660f1556454046f
nem035 <=>
2023-02-12 20:35:04 +0100

    title change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TFS - AI knowledge explorer</title>
+        <title>Tim Ferris AI knowledge explorer</title>
         <meta name="description" content="tim ferriss ai" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
