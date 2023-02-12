d541735296e869a7ae08b3c243ba6318e6e68958
nem035 <=>
2023-02-12 20:19:18 +0100

    opa

--- src/pages/api/ask-question.ts
+++ src/pages/api/ask-question.ts
@@ -181,7 +181,7 @@ export default async function handler(
     const completionResponse = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
-      max_tokens: 512,
+      max_tokens: 1024,
       temperature: 0.4,
     });
     const {
