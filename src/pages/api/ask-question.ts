6d2d746deaf5d810d8dd67b6dc0801aa6bafa269
nem035 <n.stojanovic035@gmail.com>
2023-02-14 00:30:09 +0100

    bump max tokens

--- src/pages/api/ask-question.ts
+++ src/pages/api/ask-question.ts
@@ -218,7 +218,7 @@ export default async function handler(
     const completionResponse = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
-      max_tokens: 256,
+      max_tokens: 512,
       temperature: 0.5,
     });
     const {
